312e9237ee605a48f7fa342541d12577405ba702
cryce truly <crycetruly@gmail.com>
2019-11-16 09:57:02 +0200

    bg(expenses): Validate future dates

--- expense_tracker/apps/expenses/serializer.py
+++ expense_tracker/apps/expenses/serializer.py
@@ -1,7 +1,15 @@
 from rest_framework import serializers
 from .models import Expense
+import datetime
 
 class ExpenseSerializer(serializers.ModelSerializer):
     class Meta:
         model=Expense
         fields=['id','name','amount','spent_on','description','currency',"category"]
+    def create(self,data):
+        self.validate(data)
+        return data
+    def validate(self,data):
+        if(data['spent_on']>datetime.date.today()):
+            raise serializers.ValidationError({'spent_on':'Invalid date,date must be prior or today'})
+        return data
